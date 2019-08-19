c4d1219b3f20002fb3f680c96972e491d431c63c
Elias Rosales <eliasrosales88@gmgmail.com>
2019-08-19 20:32:31 +0200

    Creating observable from "scracth" using Observable.create from rxjs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +12,21 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.firstObservableSubscription = interval( 1000 ).subscribe( count => {
-      console.log(count);
+    // this.firstObservableSubscription = interval( 1000 ).subscribe( count => {
+    //   console.log(count);
       
+    // });
+
+    const customIntervalObservable = Observable.create(( observer ) => {
+      let count = 0;
+      setInterval( () =>{
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.firstObservableSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data);
     });
   }
 
